2dcabb9f48e07d9c56419c8eb61c4c8b34ec4a48
SkyL9ne <annapeltola7@gmail.com>
2026-04-10 17:00:02 +0300

    Create Start.bat for user setup and automation
    This batch file performs several administrative tasks including deleting shortcuts, creating users, and modifying registry settings. It also uses Python to automate clicks and displays a login message.

---
+++ Start.bat
@@ -0,0 +1,26 @@
+@echo OFF
+DEL /F "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+DEL /F "C:\Users\Public\Desktop\Unity Hub.lnk"
+NET USER runneradmin TheDisa1a
+NET CONFIG server /srvcomment:"Windows Server By Disala"
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F
+NET USER TheDisala TheDisa1a /ADD >NUL 
+NET LOCALGROUP administrators TheDisala /ADD >NUL
+NET USER installer /DELETE 
+ICACLS C:\Windows\Temp /GRANT TheDisala:F >NUL 
+ICACLS C:\Windows\installer /GRANT TheDisala:F >NUL 
+diskperf -Y >NUL 
+python -c "import pyautogui as pag; pag.click(876, 21, duration=2)"
+python -c "import pyautogui as pag; pag.click(906, 73, duration=2)"
+echo ..................................................
+echo ..#####...######...####....####...##.......####...
+echo ..##..##....##....##......##..##..##......##..##..
+echo ..##..##....##.....####...######..##......######..
+echo ..##..##....##........##..##..##..##......##..##..
+echo ..#####...######...####...##..##..######..##..##..
+echo ..................................................
+echo IP:
+tasklist | find /I "ngrok.exe" >NUL && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: TheDisala / runneradmin
+echo Password: TheDisa1a
+echo You can login now!
