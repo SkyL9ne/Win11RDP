4d21d13e31597838e63b51b48340517ba0e2facb
SkyL9ne <annapeltola7@gmail.com>
2026-04-10 16:39:53 +0300

    Create Access.bat to enable RDP access
    This batch file enables Remote Desktop Protocol (RDP) access by modifying registry settings and firewall rules, and starts ngrok for tunneling.

---
+++ Access.bat
@@ -0,0 +1,8 @@
+@echo OFF
+reg ADD "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
+reg ADD "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 1 /f
+start powershell.exe -NoExit -Command "ngrok tcp 3389 --region in"
+echo "Successfully Enabled RDP Access..."
+
+REM wall.bat
