e305d1179ccf758757ab11e2bf4a108753bcec69
SkyL9ne <annapeltola7@gmail.com>
2026-04-10 16:47:26 +0300

    Refactor Wall.bat for improved command syntax

--- Wall.bat
+++ Wall.bat
@@ -1,14 +1,14 @@
-@echo off
-powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
-powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg -OutFile CachedImage_1024_768_POS4.jpg"
+@echo OFF 
+powershell.exe -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
+powershell.exe -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg -OutFile CachedImage_1024_768_POS4.jpg"
 
-set "TranscodedWallpaper=TranscodedWallpaper"
-set "CachedImage=CachedImage_1024_768_POS4.jpg"
+SET "TranscodedWallpaper=TranscodedWallpaper"
+SET "CachedImage=CachedImage_1024_768_POS4.jpg"
 
-set "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
-set "cachedFileDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles"
+SET "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
+SET "cachedFileDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles"
 
-copy /y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper"
-copy /y "%CachedImage%" "%cachedFileDir%\CachedImage_1024_768_POS4.jpg"
+COPY /Y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper"
+COPY /Y "%CachedImage%" "%cachedFileDir%\CachedImage_1024_768_POS4.jpg"
 
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
